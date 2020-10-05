b65dfbc6233657e2c6e4712698d6d38a76f068bf
Bohdan <bshmalko97@gmail.com>
2020-10-05 22:57:15 +0300

    simple design profile page

--- frontend/mes-music/src/components/Profile/ProfileContainer.jsx
+++ frontend/mes-music/src/components/Profile/ProfileContainer.jsx
@@ -7,8 +7,11 @@ const someUser = {
     photo: 'https://dwpdobr8xeaso.cloudfront.net/wgvs-images/avatars/profile_llama@4x.png',
     nickName: 'Vova666',
     status: 'MC Llama - best!!!',
-    info: 'The llama (/ˈlɑːmə/; Spanish pronunciation: [ˈʎama]) (Lama glama) is a domesticated South American camelid, widely used as a meat and pack animal by Andean cultures since the Pre-Columbian era.',
-    posts: ['My coat is white!', 'And now black!']
+    info: 'he llama (/ˈlɑːmə/; Spanish pronunciation: [ˈʎama]) (Lama glama) is a domesticated South American camelid, widely used as a meat and pack animal by Andean cultures since the Pre-Columbian era.',
+    posts: [
+        {header: 'white coat', info : 'My coat is white!blablablablablablablablabla blablablablablablablablablablablablablablablablablabl ablablablablablablabl ablablablablablablabl ablablablablablablablablablablablablablabl ablablablablabla'},
+        {header: 'black coat', info : 'And now black!'}
+        ]
 }
 
 class ProfileContainer extends React.Component {
