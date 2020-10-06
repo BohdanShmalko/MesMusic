4b38ae11efcab907f012ab0398b8dc8bfc4d51d5
Bohdan <bshmalko97@gmail.com>
2020-10-06 16:26:30 +0300

    some cosmetic profile

--- frontend/mes-music/src/components/Profile/ProfileContainer.jsx
+++ frontend/mes-music/src/components/Profile/ProfileContainer.jsx
@@ -7,7 +7,7 @@ const someUser = {
     photo: 'https://dwpdobr8xeaso.cloudfront.net/wgvs-images/avatars/profile_llama@4x.png',
     nickName: 'Vova666',
     status: 'MC Llama - best!!!',
-    info: 'he llama (/ˈlɑːmə/; Spanish pronunciation: [ˈʎama]) (Lama glama) is a domesticated South American camelid, widely used as a meat and pack animal by Andean cultures since the Pre-Columbian era.',
+    info: 'he llama (/ˈlɑːmə/; Spanish pronunciation: [ˈʎama]) (Lama glama) is a domesticated South American camelid, widely used as a meat and pack animal by Andean cultures since the Pre-Columbian era. ablablablablablablabl ablablablablablablablablablablablablablabl ablablablablablablabl ablablablablablablablablablablablablablabl ablablablablablablabl ablablablablablablablablablablablablablablsablablablablablablabl ablablablablablablablablablablablablablabl ablablablablablablabl ablablablablablablablablablablablablablabl ablablablablablablabl ablablablablablablablablablablablablablabl ablablablablablablabl ablablablablablablablablablablablablablabl ablablablablablablabl ablablablablablablablablablablablablablabl',
     posts: [
         {header: 'white coat', info : 'My coat is white!blablablablablablablablabla blablablablablablablablablablablablablablablablablabl ablablablablablablabl ablablablablablablabl ablablablablablablablablablablablablablabl ablablablablabla'},
         {header: 'black coat', info : 'And now black!'}
