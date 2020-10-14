22f3cd6405c58ba97cdf17acd7b2181ae420172b
Kolmagin Danil <dkolmagin@gmail.com>
2020-10-14 10:14:15 +0300

    A bit repair commit

--- frontend/mes-music/src/components/Profile/SidePhotoBar/SidePhotoBar.jsx
+++ frontend/mes-music/src/components/Profile/SidePhotoBar/SidePhotoBar.jsx
@@ -3,14 +3,14 @@ import s from './SidePhotoBar.module.css'
 
 export const SidePhotoBar = ({photos}) => {
 
-    let [photoNumber, setPhotoNumber] = useState(0)
-    let [AllowedToRun, setAllowedToRun] = useState(true)
+    const [photoNumber, setPhotoNumber] = useState(0)
+    const [AllowedToRun, setAllowedToRun] = useState(true)
 
     const RECOVERY_TIME = 3000
     const TIME_FOR_ALLOWED = 10000
     const PHOTO_QUANTITY = 5
 
-    const onClick = (value) => {
+    const onChangePhoto = (value) => {
         setAllowedToRun(false)
         setPhotoNumber(photoNumber + value)
         setTimeout(() => setAllowedToRun(true), TIME_FOR_ALLOWED)
@@ -18,32 +18,25 @@ export const SidePhotoBar = ({photos}) => {
 
     useEffect(() => {
         if (AllowedToRun) {
-            const interval = setInterval(() => setPhotoNumber(photoNumber++), RECOVERY_TIME)
+            const interval = setInterval(() => setPhotoNumber((photoNumber + 1)), RECOVERY_TIME)
             return () => clearInterval(interval)
         }
-    }, [AllowedToRun]);
+    });
 
     return <div className={s.sidePhotoBar}>
-        <button onClick={() => onClick(-1)} className={s.button}>/////\\\\\</button>
-        <div>
-            {(() => {
-                const map = []
-                for (let i = photoNumber; i < photoNumber + PHOTO_QUANTITY; i++) {
-                    let item
-                    if (i < 0) {
-                        item = photos[((i + 1) % photos.length) + photos.length - 1]
-                    } else {
-                        item = photos[i % photos.length]
-                    }
-                    map.push(
-                        <div key={i} className={s.photos}>
-                            <img src={item} alt="!"/>
-                        </div>
-                    )
-                }
-                return map
-            })()}
+        <button onClick={() => onChangePhoto(-1)} className={s.button}>/////\\\\\</button>
+        <div className={s.photoBlock}>
+            {new Array(PHOTO_QUANTITY).fill(0)
+                .map((value, index) => {
+                    let item, i = photoNumber + index
+                    i < 0
+                        ? item = photos[((i + 1) % photos.length) + photos.length - 1]
+                        : item = photos[i % photos.length]
+                    return <div key={i} className={s.photo}>
+                        <img src={item} alt="!"/>
+                    </div>
+                })}
         </div>
-        <button onClick={() => onClick(+1)} className={s.button}>\\\\\/////</button>
+        <button onClick={() => onChangePhoto(+1)} className={s.button}>\\\\\/////</button>
     </div>
 }
