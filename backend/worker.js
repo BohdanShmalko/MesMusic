995fff8e48296f5aecc2c39ef2c130ed5d533c4b
Kolmagin Danil <dkolmagin@gmail.com>
2020-11-06 11:53:56 +0200

    Trash #2

--- backend/worker.js
+++ backend/worker.js
@@ -1,15 +1,27 @@
 const createAppWithSettings = require('./functions/createAppWithSettings')
 const getJSON = require('./functions/getJSON')
-const query = require('./functions/query')
+//const query = require('./functions/query')
+const {Pool} = require('pg')
 
 const app = createAppWithSettings()
 const PORT = 3001
 
+
+const pool = new Pool({
+  host: '127.0.0.1',
+  port: 5432,
+  database: 'avecoder',
+  user: 'postgres',
+  password: '0000',
+})
+
 const testDanil = getJSON('test')
 
 app.get('/gradebook/:number', (req, res) => {
-
-  res.send(JSON.stringify(testDanil.messages[req.params.number]))
+  const sql = 'SELECT * FROM employee;';
+  pool.query(sql)
+      .then((res1) => res.send(JSON.stringify(res1.rows)))
+      .then(err => {if(err) throw err})
 })
 
 app.post('/saveMarks', (req, res) => {
